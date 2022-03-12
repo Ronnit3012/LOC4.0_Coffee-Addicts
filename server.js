39a87bc9d3c388ffd24be560fb321ba64f2e3938
Ronnit3012 <ronnit.mirgh@gmail.com>
2022-03-12 20:01:03 +0530

    body align in home

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // Importing modules
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+// const data = require('./data/data');
 
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
 // Define paths for Express Configuration
 const publicDirectoryPath = path.join(__dirname, 'public');
@@ -12,7 +14,11 @@ const publicDirectoryPath = path.join(__dirname, 'public');
 // Setup Static Directory to serve
 app.use(express.static(publicDirectoryPath));  // this becomes our main directory on our local host
 
+// console.log(data.loadNotes());
+
+console.log(fs.readFileSync(userData.json));
+
 // Server call
-app.listen(5000, () => {
-    console.log('Server is up on port 5000');
-})
+// app.listen(5000, () => {
+//     console.log('Server is up on port 5000');
+// })
